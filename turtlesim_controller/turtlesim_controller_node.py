20085befddd7494cf508b998b10ea015548c5c7f
Álvaro Fraile Carmena <afrailecarmena@gmail.com>
2024-11-10 10:38:01 +0100

    Added toggle drawing and clear drawing functionality

--- turtlesim_controller/turtlesim_controller_node.py
+++ turtlesim_controller/turtlesim_controller_node.py
@@ -2,7 +2,8 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Char
+from std_srvs.srv import Empty  # Para el servicio de limpieza de trazos
+from turtlesim.srv import SetPen  # Servicio para cambiar el color de la traza
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
@@ -12,9 +13,22 @@ class TurtlesimController(Node):
 
         self.speed = 1
         self.rotation_speed = 1
+        self.drawing_enabled = True  # Variable para alternar el estado del dibujo
+        self.clear_drawn_lines = False
 
         self.char_subscriber = self.create_subscription(String, "keyboard", self.process_action, 10)
         self.ts_publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+
+        # Cliente del servicio de limpieza
+        self.clear_client = self.create_client(Empty, '/clear')
+        while not self.clear_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Esperando a que el servicio /clear esté disponible...')
+
+        # Cliente del servicio para cambiar el color de la traza
+        self.set_pen_client = self.create_client(SetPen, '/turtle1/set_pen')
+        while not self.set_pen_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Esperando a que el servicio /turtle1/set_pen esté disponible...')
+
         self.get_logger().info("Controlador Turtlesim creado")
 
     def process_action(self, msg):
@@ -38,10 +52,17 @@ class TurtlesimController(Node):
         if "d" in characters:
             rotation += -self.rotation_speed
             self.get_logger().info("Rotate Right")
+
         if " " in characters:
-            self.toggle_drawing()
+            self.toggle_drawing(True)
+        else:
+            self.toggle_drawing(False)
+
         if "c" in characters:
-            self.clear_drawn()
+            self.clear_drawn(True)
+        else:
+            self.clear_drawn(False)
+
         if "r" in characters:
             self.reset_position()
 
@@ -50,11 +71,36 @@ class TurtlesimController(Node):
 
         self.ts_publisher.publish(twist)
 
-    def toggle_drawing(self):
-        pass
+    def toggle_drawing(self, status):
+        # Alterna el estado de dibujo
+        self.drawing_enabled = status
 
-    def clear_drawn(self):
-        pass
+        request = SetPen.Request()
+
+        if not self.drawing_enabled:
+            # Cambia el color de la traza al color de fondo (simula que está desactivada)
+            request.r = 69
+            request.g = 86
+            request.b = 255
+        else:
+            # Restaura el color de la traza (blanco por defecto)
+            request.r = 255
+            request.g = 255
+            request.b = 255
+
+        # Parámetros adicionales para mantener el ancho de la traza y activar la traza
+        request.width = 2
+        request.off = False
+        self.set_pen_client.call_async(request)
+
+    def clear_drawn(self, status):
+        self.clear_drawn_lines = status
+
+        if(self.clear_drawn_lines):
+            # Llama al servicio de limpieza
+            request = Empty.Request()
+            self.clear_client.call_async(request)
+            self.get_logger().info("Limpieza de trazos")
 
     def reset_position(self):
         pass
